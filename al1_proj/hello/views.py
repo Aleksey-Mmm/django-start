d2d20574006fa72da196bef34a98f7fedc2db3eb
Alexey_Mmm <alex@proplus.ru>
2023-08-23 16:23:21 +0900

    configure

--- al1_proj/hello/views.py
+++ al1_proj/hello/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello blin! </br> Главная..")
+
+
+def about(request):
+    return HttpResponse("О сайте")
+
+
+def contact(response):
+    return HttpResponse("Контакты")
