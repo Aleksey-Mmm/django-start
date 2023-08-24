94bc31a1b5db248a5f2ffd72ee2f171d9abc6548
Alexey_Mmm <alex@proplus.ru>
2023-08-24 15:17:30 +0900

    paths and parameters

--- al1_proj/al1_proj/urls.py
+++ al1_proj/al1_proj/urls.py
@@ -15,11 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from hello import views
 
+product_patterns = [
+    path("", views.products),
+    path("new", views.new),
+    path("top", views.top),
+]
+
 urlpatterns = [
+    path("user_get/", views.user_get),
+    re_path(r"products/(?P<id>\d+)/", include(product_patterns)),
     path("", views.index),
-    re_path(r"^about", views.about),
+    re_path(r"^user3/(?P<name>\D+)/(?P<age>\d+)", views.user3),
+    re_path(r"^user3/(?P<name>\D+)", views.user3),
+    re_path(r"^user3", views.user3),
+    path("user/<str:name>", views.user),
+    path("user2", views.user2),
+    path("user2/<name>", views.user2),
+    path("user2/<name>/<int:age>", views.user2),
+    path("index", views.index),
+    path("zag3", views.zag_3),
+    path("zag2", views.zag_2),
+    re_path(r"^zag", views.zag),
+    re_path(r"^about", views.about, kwargs={"name":"Alex", "age": 50}),
     re_path(r"^contact", views.contact),
 ]
--- al1_proj/hello/views.py
+++ al1_proj/hello/views.py
@@ -1,13 +1,56 @@
 from django.http import HttpResponse
 
+def products(request, id=0):
+    return HttpResponse(f"Список товаров {id}")
+
+def new(request, id=0):
+    return HttpResponse(f"Новые товары {id}")
+
+def top(request, id=0):
+    return HttpResponse(f"Популярные товары {id}")
+
+def user_get(request):
+    name = request.GET.get("name", "Nemo")
+    age = request.GET.get("age", 0)
+    return HttpResponse(f"<h2>Имя: {name}; Возраст: {age}</h2>")
+
+def user(request, name):
+    return HttpResponse(f"<h2>Имя: {name}</h2>")
+
+def user2(request, name="Nemo", age=0):
+    return HttpResponse(f"<h2>Имя: {name}; Возраст: {age}</h2>")
+
+def user3(request, name="Nemo", age=0):
+    return HttpResponse(f"<h2>Имя: {name}; Возраст: {age}</h2>")
+
+def zag(request):
+    return HttpResponse("HEllo All!", headers={"SecretCode": "2123543"})
+
+def zag_2(request):
+    return HttpResponse("ErroR occured", status=400, reason="Incorrect data")
+
+def zag_3(request):
+    return HttpResponse("<h1> Plain text </h1>", content_type="text/plain", charset="utf-8")
 
 def index(request):
-    return HttpResponse("Hello blin! </br> Главная..")
+    host = request.META["HTTP_HOST"] # получаем адрес сервера
+    user_agent = request.META["HTTP_USER_AGENT"] # данные браузера
+    path = request.path # запрошеный путь
+    return HttpResponse(f"""
+                        <h2> О запросе </h2>
+                        <p>Хост: {host} </p>
+                        <p>Браузер {user_agent}</p>
+                        <p>Путь {path}</p>
+                        """)
 
 
-def about(request):
-    return HttpResponse("О сайте")
+def about(request, name, age):
+    return HttpResponse(f"""
+                        <h2> О пользователе </h2>
+                        <p>Имя: {name} </p>
+                        <p>Возраст {age}</p>
+                        """)
 
 
-def contact(response):
+def contact(request):
     return HttpResponse("Контакты")
